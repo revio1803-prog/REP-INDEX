ed0d55910c1e1fa60078e32c3c4d9474fe0e9578
revio1803-prog <revio1803@gmail.com>
2026-03-15 22:41:16 +0530

    Update identifiers.js

--- routes/identifiers.js
+++ routes/identifiers.js
@@ -4,6 +4,70 @@ const router = express.Router();
 const pool = require("../db/db");
 const layout = require("../views/layout");
 
+/* ---------- CREATE IDENTIFIER PAGE ---------- */
+
+router.get("/create",(req,res)=>{
+
+res.send(layout("Create Identifier",`
+
+<h2>Create Research Identifier</h2>
+
+<form method="POST" action="/create">
+
+Title
+<input name="title" required>
+
+Year
+<input name="year" required>
+
+Resource URL
+<input name="url" required>
+
+Author ID
+<input name="author_id">
+
+<button>Create Identifier</button>
+
+</form>
+
+`));
+
+});
+
+
+/* ---------- CREATE IDENTIFIER ---------- */
+
+router.post("/create", async (req,res)=>{
+
+const {title,url,year,author_id} = req.body;
+
+const result = await pool.query(
+"SELECT COUNT(*) FROM identifiers"
+);
+
+const number = parseInt(result.rows[0].count) + 1;
+
+const id = `RID-${new Date().getFullYear()}-${String(number).padStart(5,"0")}`;
+
+await pool.query(
+"INSERT INTO identifiers (id,title,url,year,author_id) VALUES ($1,$2,$3,$4,$5)",
+[id,title,url,year,author_id]
+);
+
+res.send(layout("Identifier Created",`
+
+<h2>Identifier Created</h2>
+
+<p><b>${id}</b></p>
+
+<a href="/rid/${id}">
+<button>Open Record</button>
+</a>
+
+`));
+
+});
+
 /* Browse Identifiers */
 
 router.get("/browse", async (req,res)=>{
