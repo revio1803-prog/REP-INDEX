c1d729b734ad856d3db58fcffee7810420b47770
revio1803-prog <revio1803@gmail.com>
2026-03-15 22:05:13 +0530

    Update idGenerator.js

--- utils/idGenerator.js
+++ utils/idGenerator.js
@@ -1,36 +1,40 @@
 const pool = require("../db/db");
 
-async function generateRID(){
+async function generateRID() {
 
-const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
 
-const result = await pool.query("SELECT COUNT(*) FROM identifiers");
+  const result = await pool.query("SELECT COUNT(*) FROM identifiers");
 
-const number = parseInt(result.rows[0].count)+1;
+  const number = parseInt(result.rows[0].count) + 1;
 
-return `RID-${year}-${String(number).padStart(5,"0")}`;
+  return `RID-${year}-${String(number).padStart(5,"0")}`;
 }
 
-async function generateAID(){
+async function generateAID() {
 
-const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
 
-const result = await pool.query("SELECT COUNT(*) FROM authors");
+  const result = await pool.query("SELECT COUNT(*) FROM authors");
 
-const number = parseInt(result.rows[0].count)+1;
+  const number = parseInt(result.rows[0].count) + 1;
 
-return `AID-${year}-${String(number).padStart(5,"0")}`;
+  return `AID-${year}-${String(number).padStart(5,"0")}`;
 }
 
-async function generateDID(){
+async function generateDID() {
 
-const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
 
-const result = await pool.query("SELECT COUNT(*) FROM datasets");
+  const result = await pool.query("SELECT COUNT(*) FROM datasets");
 
-const number = parseInt(result.rows[0].count)+1;
+  const number = parseInt(result.rows[0].count) + 1;
 
-return `DID-${year}-${String(number).padStart(5,"0")}`;
+  return `DID-${year}-${String(number).padStart(5,"0")}`;
 }
 
-module.exports = {generateRID,generateAID,generateDID};
+module.exports = {
+  generateRID,
+  generateAID,
+  generateDID
+};
