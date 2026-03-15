232a795ae45d4b76301e95656e426d8d72699282
revio1803-prog <revio1803@gmail.com>
2026-03-15 18:24:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,24 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const database = {
+  "RID-2026-0001": "https://example.com/research-paper",
+  "RID-2026-0002": "https://example.com/article2"
+};
+
 app.get("/", (req, res) => {
-  res.send("REP Index running successfully");
+  res.send("REP Index system running");
+});
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const link = database[id];
+
+  if (link) {
+    res.redirect(link);
+  } else {
+    res.send("Identifier not found");
+  }
 });
 
 app.listen(PORT, () => {
